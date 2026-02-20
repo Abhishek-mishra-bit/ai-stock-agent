87dc09d2b000f3c269b115e40f97ae85a54e07ed
Abhishek-mishra-bit <abhishekmi1249@gmail.com>
2026-02-21 01:44:40 +0530

    fetching data from nse like stock quote, market indices, historical data etc.

--- nseServiceTest.js
+++ nseServiceTest.js
@@ -3,13 +3,13 @@ const NSEService = require("./src/services/nseService");
 async function runtest() {
   const nseService = new NSEService();
   try {
-    // console.log("Fetching stock quote for ITC...");
-    // const stockQuote = await nseService.getStockQuote("ITC");
-    // console.log("Stock Quote for ITC: ", stockQuote);
+    console.log("Fetching stock quote for ITC...");
+    const stockQuote = await nseService.getStockQuote("ITC");
+    console.log("Stock Quote for ITC: ", stockQuote);
 
-    // console.log("\n Fetching market indices...");
-    // const marketIndices = await nseService.getMarketIndices();
-    // console.log("Market Indices: ", marketIndices);
+    console.log("\n Fetching market indices...");
+    const marketIndices = await nseService.getMarketIndices();
+    console.log("Market Indices: ", marketIndices);
 
     console.log("\n Fetching historical data for ITC...");
     const history = await nseService.getHistoricalData("ITC", 30);
